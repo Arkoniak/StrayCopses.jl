52f33e69d6eca056233f11fe1159fe80be5fdc83
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-03 20:07:42 +0000

    build based on a0fc81f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StrayCopses.jl-1","page":"Home","title":"StrayCopses.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [StrayCopses]","category":"page"}]
+}
