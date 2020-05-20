e32f143ba06572b7323611c008ce3cf9da120a3e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-20 19:08:01 +0000

    build based on 031ce7d

---
+++ v0.0.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StrayCopses.jl-1","page":"Home","title":"StrayCopses.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [StrayCopses]","category":"page"},{"location":"#StrayCopses.feature_best_split-NTuple{5,Any}","page":"Home","title":"StrayCopses.feature_best_split","text":"feature_best_split\n\nFor a given feature search best split value.\n\n\n\n\n\n","category":"method"}]
+}
